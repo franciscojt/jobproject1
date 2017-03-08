026d4e4b5f000384b418a5bc3f4f6ef8d0f5dd13
Francisco J Trujillo <francisco.jose1982@gmail.com>
2017-03-08 09:55:35 -0600

    remove console logs

--- project1.js
+++ project1.js
@@ -1,4 +1,3 @@
-console.log("js is loaded")
 window.onload = function() {
                 var data = document.getElementById('data');
 				console.log(data)
@@ -13,19 +12,18 @@ function call(arr) {
 		var td1 = document.createElement("td");
 		var td2 = document.createElement("td");
 		var td3 = document.createElement("td");
-		var fname = document.createTextNode(arr[i].first_name)
-		var lname = document.createTextNode(arr[i].last_name)
-		var home = document.createTextNode(arr[i].home)
+		var fname = document.createTextNode(arr[i].first_name);
+		var lname = document.createTextNode(arr[i].last_name);
+		var home = document.createTextNode(arr[i].home);
 		
-		tr.appendChild(td1)
-		td1.appendChild(fname)
-		tr.appendChild(td2)
-		td2.appendChild(lname)
-		tr.appendChild(td3)
-		td3.appendChild(home)
-		data.appendChild(tr)
-		console.log(tr, i)
-		console.log(data, i)
+		tr.appendChild(td1);
+		td1.appendChild(fname);
+		tr.appendChild(td2);
+		td2.appendChild(lname);
+		tr.appendChild(td3);
+		td3.appendChild(home);
+		data.appendChild(tr);
+
 	}
 
 }
@@ -61,15 +59,7 @@ var table_data = [{
 	first_name: 'Susan',
 	last_name: 'Foreman',
 	home: 'Gallifrey'}
-]
+];
 window.onload = function(){
 	call(table_data);
 }
-//.innerHTML = "new Text"
-// for(var i = 0; i < table_data.length; i++ ){
-// 	document.write("<td>" + table_data[i].first_name + "</td>")}
-
-// 	var bod = document.body;
-// for (var i = 0; i < 10; i ++){
-//   bod. innerHTML += "<li>This has gone through the loop completely: " +i+ " times</li>";
-// }
